fe575139a9732cf2fe8603cfaaf538a32f3cb04a
3001vaf-maker <30.01vaf@gmail.com>
2026-08-23 19:45:03 +0300

    ui: rename bottom Home button to Settings

--- price-nav-fix.js
+++ price-nav-fix.js
@@ -2,6 +2,10 @@
   const fixPriceNavigation = () => {
     document.querySelectorAll('.content button[data-page="home"]').forEach(button => button.remove());
     document.querySelectorAll('.bottom button[data-page="services"]').forEach(button => button.remove());
+    document.querySelectorAll('.bottom button[data-page="home"]').forEach(button => {
+      const br = button.querySelector('br');
+      if (br) br.nextSibling.textContent = 'Настройки';
+    });
   };
 
   fixPriceNavigation();

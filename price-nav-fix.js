f05cdc74b395774240ab13ff08374bf327823a59
3001vaf-maker <30.01vaf@gmail.com>
2026-08-23 19:39:04 +0300

    feat: move Price entry from bottom navigation to master home

--- price-nav-fix.js
+++ price-nav-fix.js
@@ -1,10 +1,11 @@
 (() => {
-  const removeInternalHomeLink = () => {
+  const fixPriceNavigation = () => {
     document.querySelectorAll('.content button[data-page="home"]').forEach(button => button.remove());
+    document.querySelectorAll('.bottom button[data-page="services"]').forEach(button => button.remove());
   };
 
-  removeInternalHomeLink();
-  new MutationObserver(removeInternalHomeLink).observe(document.getElementById('app'), {
+  fixPriceNavigation();
+  new MutationObserver(fixPriceNavigation).observe(document.getElementById('app'), {
     childList: true,
     subtree: true
   });

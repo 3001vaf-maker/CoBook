93ff3a65eec5b0afaa7b128dc2214b6c3edc7bae
3001vaf-maker <30.01vaf@gmail.com>
2026-08-25 21:51:45 +0300

    Create MAINE module

---
+++ app/maine/maine.js
@@ -0,0 +1,4 @@
+(function(){
+ function render(){return shell(`<section class="hero"><div class="eyebrow">КАБИНЕТ МАСТЕРА</div><h1>Главная</h1><button class="primary full" data-action="navigate" data-page="clients" type="button">Клиенты</button></section>`)}
+ CoBook.modules.maine={render,handle(){}};
+})();

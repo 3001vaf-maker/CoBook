c92cab88bfabc19a29aeffade4790749740dfc04
3001vaf-maker <30.01vaf@gmail.com>
2026-08-27 10:04:03 +0300

    fix: start application on MAINE

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 /* CoBook entry point: startup only. All screen logic belongs to app/ MODULEs. */
 (function(){
   if(!window.CoBook||!window.CoBook.modules)throw new Error('CoBook core is not loaded');
-  window.CoBook.start=window.CoBook.start||function(){window.state.page='journal';window.render()};
+  window.CoBook.start=window.CoBook.start||function(){window.state.page='maine';window.state.maineView='main';window.render()};
   window.CoBook.start();
 })();
